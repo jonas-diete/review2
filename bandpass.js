c07f5ef78331626eeb6ee73f52cbb416d62c2762
Jonas Diete <jonas.diete@gmail.com>
2022-10-25 10:42:19 +0100

    testing for 0 and negative throws errors

--- bandpass.js
+++ bandpass.js
@@ -1,7 +1,12 @@
 const bandpass = (track, lowerLimit, upperLimit) => {
-  if (lowerLimit === 0) {
+  if (lowerLimit <= 0 || upperLimit <= 0) {
     throw new Error('ValueError');
   }
+  track.forEach((frequency) => {
+    if (frequency <= 0) {
+      throw new Error('ValueError');
+    }
+  })
   return track;
 }
 
--- bandpass.test.js
+++ bandpass.test.js
@@ -5,7 +5,26 @@ describe('Bandpass', () => {
     expect(bandpass([30,10,45,40,20],20,50)).toEqual([30,10,45,40,20]);
   });
 
-  it('throws error when a number is 0 or negative', () => {
+  it('throws error when a lowerLimit is 0', () => {
     expect(() => {bandpass([30,20,45,40,20],0,50)}).toThrow('ValueError');
+  });
+
+  it('throws error when a lowerLimit is negative', () => {
+    expect(() => {bandpass([30,20,45,40,20],-14,50)}).toThrow('ValueError');
+  });
+
+  it('throws error when a upperLimit is negative', () => {
+    expect(() => {bandpass([30,20,45,40,20],20,-40)}).toThrow('ValueError');
+  });
+
+  it('throws error when a upperLimit is 0', () => {
+    expect(() => {bandpass([30,20,45,40,20],20,0)}).toThrow('ValueError');
+  });
+  it('throws error when a frequency is 0', () => {
+    expect(() => {bandpass([0,20,45,40,20],20,30)}).toThrow('ValueError');
+  });
+
+  it('throws error when a frequency is negative', () => {
+    expect(() => {bandpass([10,20,-13,40,20],20,30)}).toThrow('ValueError');
   });
 });
