252370dcd3eecdc2c376c5ff119e03c51411f5a6
Jonas Diete <jonas.diete@gmail.com>
2022-10-25 10:35:41 +0100

    value error added

--- bandpass.js
+++ bandpass.js
@@ -1,4 +1,7 @@
 const bandpass = (track, lowerLimit, upperLimit) => {
+  if (lowerLimit === 0) {
+    throw new Error('ValueError');
+  }
   return track;
 }
 
--- bandpass.test.js
+++ bandpass.test.js
@@ -4,4 +4,8 @@ describe('Bandpass', () => {
   it('should return the track unchanged', () => {
     expect(bandpass([30,10,45,40,20],20,50)).toEqual([30,10,45,40,20]);
   });
+
+  it('throws error when a number is 0 or negative', () => {
+    expect(() => {bandpass([30,20,45,40,20],0,50)}).toThrow('ValueError');
+  });
 });
